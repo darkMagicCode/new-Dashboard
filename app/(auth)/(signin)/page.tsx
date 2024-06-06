fb180879b440e6efd9ef1c29ff9e1dc31f8b96b4
ayman nawwar <darkmagiccode.com>
2024-06-06 19:24:19 +0300

    fix

--- app/(auth)/(signin)/page.tsx
+++ app/(auth)/(signin)/page.tsx
@@ -2,14 +2,13 @@ import { Metadata } from 'next';
 import Link from 'next/link';
 import UserAuthForm from '@/components/forms/user-auth-form';
 import { buttonVariants } from '@/components/ui/button';
-import  logo  from './logo.jpg';
 import { cn } from '@/lib/utils';
 
  const metadata: Metadata = {
   title: 'Al-hazam car rental',
   description: 'Al-hazam car rental', 
   icons: {
-    icon: logo
+    icon: './logo.jpg'
   }
 };
 
